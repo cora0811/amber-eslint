cbf406e5b3355ea9360849dcb15889962edd1d09
jinming08Li <cora@eforest.finance>
2023-05-18 15:47:03 +0800

    update

--- bin/amber-lint.js
+++ bin/amber-lint.js
@@ -29,33 +29,26 @@ function onFatalError(error) {
     const { exec } = require('child_process');
 
 
-    const unPlugins = 'npm uninstall eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks husky lint-staged prettier stylelint stylelint-config-prettier stylelint-config-standard eslint-plugin-no-inline-styles @commitlint/cli @commitlint/config-conventional';
-    const plugins = 'npm install --save-dev eslint@^8.23.1 eslint-config-prettier@^6.11.0 eslint-plugin-prettier@^4.2.1 eslint-plugin-react@^7.31.8 eslint-plugin-react-hooks@^4.2.0 husky@^8.0.1 lint-staged@13.2.2 prettier@^2.2.1 stylelint@^14.9.1 stylelint-config-prettier@^9.0.3 stylelint-config-standard@^26.0.0 eslint-plugin-no-inline-styles@^1.0.5 @commitlint/cli@^17.0.2 @commitlint/config-conventional@^17.0.2';
+    // const unPlugins = 'npm uninstall eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks husky lint-staged prettier stylelint stylelint-config-prettier stylelint-config-standard eslint-plugin-no-inline-styles @commitlint/cli @commitlint/config-conventional';
+    const plugins = 'yarn add eslint@^8.23.1 eslint-config-prettier@^6.11.0 eslint-plugin-prettier@^4.2.1 eslint-plugin-react@^7.31.8 eslint-plugin-react-hooks@^4.2.0 husky@^8.0.1 lint-staged@13.2.2 prettier@^2.2.1 stylelint@^14.9.1 stylelint-config-prettier@^9.0.3 stylelint-config-standard@^26.0.0 eslint-plugin-no-inline-styles@^1.0.5 @commitlint/cli@^17.0.2 @commitlint/config-conventional@^17.0.2';
 
-    // Uninstall the lint plugins
-    exec(unPlugins, (err, stdout, stderr) => {
+    // Install the lint plugins
+    exec(plugins, (err, stdout, stderr) => {
       if (err) {
         console.error(err);
         return;
       }
-      // Install the lint plugins
-      exec(plugins, (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log('\n\nPlugins has been installed for your project!\n\n');
+      console.log('\n\nPlugins has been installed for your project!\n\n');
 
-        // Generate Configuration File
-        eslint()
-        prettier()
-        stylelint()
-        commitlint()
-        ts()
-        editor()
-        generateHusky()
-        lintStaged()
-      });
+      // Generate Configuration File
+      eslint()
+      prettier()
+      stylelint()
+      commitlint()
+      ts()
+      editor()
+      generateHusky()
+      lintStaged()
     });
   } catch (error) {
     console.error(error);
